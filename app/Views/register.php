8d9f3c549102678f37f1720e7886c9e75ef90d20
Antsistra <hadinurmuhamad8@gmail.com>
2025-03-20 08:37:01 +0700

    init

--- app/Views/register.php
+++ app/Views/register.php
@@ -3,40 +3,37 @@
 <div class="card">
     <div class="card-body login-card-body">
         <p class="login-box-msg">Register to Page</p>
-        <form action="../index3.html" method="post">
-            <div class="input-group mb-3">
-                <input type="email" class="form-control" placeholder="Name" />
-                <div class="input-group-text"><span class="bi bi-envelope"></span></div>
-            </div>
-            <div class="input-group mb-3">
-                <input type="password" class="form-control" placeholder="Password" />
-                <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
-            </div>
-            <div class="input-group mb-3">
-                <input type="password" class="form-control" placeholder="Password" />
-                <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
-            </div>
-            <div class="input-group mb-3">
-                <input type="password" class="form-control" placeholder="Password" />
-                <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
-            </div>
-            <div class="input-group mb-3">
-                <input type="password" class="form-control" placeholder="Password" />
-                <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
-            </div>
-            <!--begin::Row-->
-            <div class="row">
+
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Name" id="name" />
+            <div class="input-group-text"><span class="bi bi-envelope"></span></div>
+        </div>
+        <div class="input-group mb-3">
+            <input type="Email" class="form-control" placeholder="Email" />
+            <div class="input-group-text"><span class="bi bi-en-fill"></span></div>
+        </div>
+        <div class="input-group mb-3">
+            <input type="password" class="form-control" placeholder="Password" />
+            <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
+        </div>
+        <div class="input-group mb-3">
+            <input type="password" class="form-control" placeholder="Confirmation Password" />
+            <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
+        </div>
+
+        <!--begin::Row-->
+        <div class="row">
 
 
-                <!-- /.col -->
-                <div class="col-12 mt-4">
-                    <div class="d-grid gap-2">
-                        <button type="submit" class="btn btn-primary">Register</button>
-                    </div>
+            <!-- /.col -->
+            <div class="col-12 mt-4">
+                <div class="d-grid gap-2">
+                    <button class="btn btn-primary" onclick="register()">Register</button>
                 </div>
-                <!-- /.col -->
             </div>
-            <!--end::Row-->
+            <!-- /.col -->
+        </div>
+        <!--end::Row-->
         </form>
 
         <!-- /.social-auth-links -->
@@ -47,4 +44,13 @@
     </div>
     <!-- /.login-card-body -->
 </div>
+
+<script>
+    let name = document.getElementById('name').value;
+
+    function register() {
+        console.log(name);
+    }
+</script>
+
 <?= $this->endSection(); ?>
